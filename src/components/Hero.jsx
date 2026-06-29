f155b2d27389bad4de43c6a1b14072d9c369b6bf
nicole <nicole@allyou.srl>
2026-06-29 11:02:50 +0200

    Hero modificata

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,15 @@
 const Hero = () => {
     return (
 <>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800;900&family=Raleway:wght@300;400;700&display=swap" rel="stylesheet"></link>
 <div className="hero">
-   <div><p>UNIQUE HAIR SALON - FIRENZE</p></div>
-   <div><h2>Non facciamo solo i capelli.<br />Creaiamo identità.</h2> </div>
+    <div className="front">
+           <div className="unique"><p ><span className="line">─────</span> UNIQUE HAIR SALON <span className="line">──</span> FIRENZE <span className="line">─────</span></p></div>
+   <div className="hero-title"><h2>Non facciamo solo i <span className="capelli">capelli.</span><br />Creaiamo esperienze uniche.</h2> </div>
+   <div><p className="subtitle">L'hair styling di ultima <br />generazione</p></div>
    <div></div>
-   <div></div>
+    </div>
+
 </div>
 </>
     );
