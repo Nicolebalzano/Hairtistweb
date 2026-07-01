3e023f89e44479bcda1e2503f51a3a96b15c4687
nicole <nicole@allyou.srl>
2026-07-01 12:40:36 +0200

    hero-responsive init

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 
 const Header = () => {
     const [scrolled, setScrolled] = useState(false);
+    const [menuOpen, setMenuOpen] = useState(false);
+    
     useEffect(() => {
         const handleScroll = () => {
             if (window.scrollY > 0) {
@@ -14,7 +16,9 @@ const Header = () => {
         window.addEventListener('scroll', handleScroll);
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
-
+   useEffect(() => {
+    document.body.style.overflow = menuOpen ? "hidden" : "auto";
+    }, [menuOpen]);
     
  return (
 <>
@@ -24,7 +28,7 @@ const Header = () => {
 <span className="part1">HAIR</span>
   <span className="part2">TIST</span>
 </h1></div>
-    <div className="flex-link">
+    <div className="flex-link desktop">
         <Link to="/">Chi siamo</Link>
           <Link to="/">Servizi</Link>
             <Link to="/">Galleria</Link>
@@ -32,7 +36,38 @@ const Header = () => {
         </div>
 
     <div className="prenota-btn">  <Link to="/">Prenota</Link></div>
-</div></header>
+</div>
+    {/* HAMBURGER MOBILE */}
+    <div className="hamburger-logo">
+            <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+      {menuOpen ? "" : "☰"}
+    </div>
+     <div className="logo-hambuger">
+                <div><h1 className="logo-sm">
+<span className="part1">HAIR</span>
+  <span className="part2">TIST</span>
+</h1></div></div>
+    </div>
+
+</header>
+
+{/* menu mobile */}
+<div className={`mobile-menu ${menuOpen ? "open" : ""}`}>
+        {/* HAMBURGER MOBILE */}
+    <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+      {menuOpen ? "✕" : "☰"}
+    </div>
+    <div className="hamburger-links">
+          <Link to="/" onClick={() => setMenuOpen(false)}>Chi siamo</Link>
+  <Link to="/" onClick={() => setMenuOpen(false)}>Servizi</Link>
+  <Link to="/" onClick={() => setMenuOpen(false)}>Galleria</Link>
+  <Link to="/" onClick={() => setMenuOpen(false)}>Contatti</Link>
+    </div>
+
+
+ <div className="prenota-btn-sm ">  <Link to="/">Prenota</Link></div>
+</div>
+
 </>
  );
 }
